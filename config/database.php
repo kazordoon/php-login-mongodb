444b07d07b7f1e28ba10910a6a34ca6e4d7252fa
Felipe Barros <felipetech@tuta.io>
2020-03-11 11:48:26 -0300

    chore: Add MongoDB connection

---
+++ config/database.php
@@ -0,0 +1,30 @@
+<?php
+	require_once 'vendor/autoload.php';
+
+	$dbConfig = json_decode(file_get_contents('config/dbConfig.json'));
+
+	class Database {
+		private $mongoUri;
+		private $connection;
+		private $db;
+		private $collection;
+
+		public function __construct() {
+			global $dbConfig;
+
+			$this->mongoUri = $dbConfig->mongoUri . $dbConfig->dbName;
+			$this->connection = new MongoDB\Client($this->mongoUri);
+			
+			$dbName = $dbConfig->dbName;
+			$this->db = $this->connection->$dbName;
+
+			$this->collection = $this->db->users;
+		}
+
+		public function getCollection() {
+			return $this->collection;
+		}
+		public function setCollection($collection) {
+			$this->collection = $collection;
+		}
+	}
