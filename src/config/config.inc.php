cfcbfda469ffca09e595f08178a8837bd712d019
Felipe Barros <felipetech@tuta.io>
2020-08-11 19:31:55 -0300

    chore: Add the mail constants

--- src/config/config.inc.php
+++ src/config/config.inc.php
@@ -5,3 +5,8 @@ define('BASE_URL', 'http://localhost:8080/');
 
 define('MONGODB_URI', 'mongodb://php:phppass@php_login_db:27017/php_login');
 define('DB_NAME', 'php_login');
+
+define('MAIL_HOST', '');
+define('MAIL_PORT', '');
+define('MAIL_USERNAME', '');
+define('MAIL_PASSWORD', '');
