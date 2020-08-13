338d8ced28b3284945f15caea7110dee2a0d679b
Felipe Barros <felipetech@tuta.io>
2020-08-13 11:31:26 -0300

    chore: Add the sendRecoveryToken method for the RecoverPasswordController

--- src/App/Controllers/RecoverPasswordController.php
+++ src/App/Controllers/RecoverPasswordController.php
@@ -3,6 +3,9 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Models\User;
+use App\Providers\Mail;
+use PHPMailer\PHPMailer\Exception;
 
 class RecoverPasswordController extends Controller {
   public function index() {
@@ -26,5 +29,43 @@ class RecoverPasswordController extends Controller {
     return $this->render('recover_password', $data);
   }
 
-  public function sendRecoveryToken() {}
+  public function sendRecoveryToken() {
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+
+    $isTheEmailFieldEmpty = empty($email);
+    if ($isTheEmailFieldEmpty) {
+      $_SESSION['error'] = 'Fill in the email field.';
+      redirectTo(BASE_URL . 'recover_password');
+    }
+
+    [$user] = User::findBy(['email' => $email]);
+
+    $userNotFound = !$user;
+    if ($userNotFound) {
+      $_SESSION['error'] = 'There is no user with the provided email.';
+      redirectTo(BASE_URL . 'recover_password');
+    }
+
+    $passwordRecoveryToken = uniqid(bin2hex(random_bytes(32)));
+    $oneHour = time() + 3600;
+    $passwordTokenExpirationTime = $oneHour;
+
+    User::findByIdAndUpdate($user['_id'], [
+      'passwordRecoveryToken' => $passwordRecoveryToken,
+      'passwordTokenExpirationTime' => $passwordTokenExpirationTime
+    ]);
+
+    $passwordRecoveryLink = BASE_URL . "reset_password?email={$email}&token={$passwordRecoveryToken}";
+    $mailMessageContent = "Forgot your password ? Use this link to recover it: {$passwordRecoveryLink}";
+    $mail = new Mail;
+    $mail->setRecipient($email, $user['name']);
+    $mail->writeContent('Recover password', $mailMessageContent);
+
+    try {
+      $mail->send();
+    } catch (Exception $e) {
+      $_SESSION['error'] = 'Could not to send a password recovery link via email, try again.';
+      redirectTo(BASE_URL . 'recover_password');
+    }
+  }
 }
