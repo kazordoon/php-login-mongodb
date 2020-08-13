468419efb899c173a060f92a64c573e0fac34225
Felipe Barros <felipetech@tuta.io>
2020-08-13 10:43:51 -0300

    chore: Add the index method on the RecoverPasswordController

--- src/App/Controllers/RecoverPasswordController.php
+++ src/App/Controllers/RecoverPasswordController.php
@@ -5,7 +5,26 @@ namespace App\Controllers;
 use App\Core\Controller;
 
 class RecoverPasswordController extends Controller {
-  public function index() {}
+  public function index() {
+    $isTheUserLoggedIn = isset($_SESSION['userId']);
+
+    if ($isTheUserLoggedIn) {
+      header('Location: ' . BASE_URL);
+      exit;
+    }
+
+    $error = $_SESSION['error'] ?? null;
+
+    $data = [
+      'error' => $error
+    ];
+
+    if ($error) {
+      unset($_SESSION['error']);
+    }
+
+    return $this->render('recover_password', $data);
+  }
 
   public function sendRecoveryToken() {}
 }
