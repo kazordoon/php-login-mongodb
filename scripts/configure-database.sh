c79c85d3ee937390975ba09f3f66830133aa68a8
Felipe Barros <felipetech@tuta.io>
2023-12-16 17:41:14 +0100

    chore: Use the "mongosh" command instead "mongo"

--- scripts/configure-database.sh
+++ scripts/configure-database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker container exec -i php_login_db mongo -uroot -ptoor --authenticationDatabase admin php_login --eval "
+docker container exec -i php_login_db mongosh -u root -p toor --authenticationDatabase admin php_login --eval "
 db.users.createIndex({ email: 1 }, { unique: 1 });
 db.createUser({ user: 'php', pwd: 'phppass', roles: [ { role: 'readWrite', db: 'php_login' } ] });
 "
